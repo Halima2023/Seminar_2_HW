78bdbb37f35433808818e03972233a939b258d7a
Halima <l-ahim@mail.ru>
2023-04-16 19:35:04 +0300

    HW_2_Task_1

---
+++ HW/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// HW Task_1 Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+
+int ThirdNumber(int num)
+{
+    int dozent = num / 10;
+    int units = num % 10;
+    int result = num / 10 % 10;
+    return result;
+}
+
+Console.WriteLine("Input  number, please: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = ThirdNumber(num);
+Console.WriteLine(result);
