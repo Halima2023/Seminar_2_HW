08bc2a1c00cb7d9e50dcbb8ff9400b8329814563
Halima <l-ahim@mail.ru>
2023-04-25 16:00:06 +0300

    Task_3

---
+++ HW/HW_5/Task_3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+// минимальным элементов массива.   [3 7 22 2 78] -> 76
+
+double[] CreateRandomArray(int size, double minValue, double maxValue)
+{
+    double[] array = new double[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+
+    return array;
+}
+
+void ShowArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i]+"");
+
+    Console.WriteLine("]");
+}
+
+double GetDiffOfNumbers(int size, double min, double max)
+{
+    double[] num = new double[size];
+    double diff = 0;
+   
+    for (int i = 0; i < size; i++)
+    {
+        if (num[i] > max) max = num[i];
+
+        else if (num[i] < min) min = num[i];
+    }
+    return diff;
+}
+Console.Write("Input a quantity of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a minimum possible element: ");
+double min = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input a maximum possible element: ");
+double max = Convert.ToDouble(Console.ReadLine());
+double[] array = CreateRandomArray(size, min, max);
+ShowArray(array);
+
+
+Console.WriteLine($"Разница между максимальным и минимальным числами: {max - min}");
+
