1a366d1485be797def5075125698dc9adc9d8dbf
Halima <l-ahim@mail.ru>
2023-04-25 03:49:24 +0300

    Task_2

---
+++ HW/HW_5/Task_1/Program.cs
@@ -0,0 +1,50 @@
+﻿// HW_5_Task_1 Задача 34: Задайте массив заполненный случайными положительными трёхзначными  числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.[345, 897, 568, 234] -> 2
+
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(99, 999);
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int GetEvenDidits(int[]myArray)
+{ int[] myArray = new int[myArray.Length];
+    int evenNum = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i] % 2 == 0)
+            evenNum = evenNum + 1;
+    }
+    return evenNum;
+}
+
+
+Console.WriteLine("Input a quantity of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myAarray); 
+
+ Console.Write($"Количество чётных чисел в массиве + " ");
+
+
+
+
---
+++ HW/HW_5/Task_2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях. [3, 7, 23, 12] -> 19      [-4, -6, 89, 6] -> 0
+
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int GetSumOfNegatives(int[] array)
+{
+    int odd = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+
+        odd += array[i];
+    return odd;
+}
+
+Console.WriteLine("Input a quantity of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
+
+Console.Write("Sum of odd indeks is " + GetSumOfNegatives(myArray));
