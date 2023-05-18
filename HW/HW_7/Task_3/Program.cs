2cb85622570db549777afa772482084c6dd10b47
Halima <l-ahim@mail.ru>
2023-05-19 00:12:06 +0300

    Task_2

---
+++ HW/HW_7/Task_3/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+// элементов в каждом столбце.
+/*
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int[,] GreateRandom2dArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+   
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(rows, columns);
+       
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)  
+           
+            Console.Write(array[i, j] + " ");
+           
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Input a quantity of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a quantity of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+GreateRandom2dArray(rows, columns);
+
