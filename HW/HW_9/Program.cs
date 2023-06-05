73be2b9e9b05e7610801f0bb5bf522f13b60d117
Halima <l-ahim@mail.ru>
2023-06-06 01:13:59 +0300

    Task_1

---
+++ HW/HW_9/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+/*
+void ShowNums3(int num)
+{
+     Console.Write(num + ", ");
+    if (num > 1) ShowNums3(num - 1);
+}
+ShowNums3(8);
+
+//Task_2. Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+
+
+        int SumOfDigits (int num1, int num2)
+        {
+            int m = num1;
+            int n = num2;
+            if (m > 0)  return ((m + n)*(n - m + 1) / 2);
+            return 0;
+        }
+
+    Console.WriteLine(SumOfDigits(1, 15));
+   
+   Task_3 Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+   Даны два неотрицательных числа m и n.
+   m = 2, n = 3 -> A(m,n) = 9
+   m = 3, n = 2 -> A(m,n) = 29
+   */
+  
+int FuncOfAkkerman(int m, int n)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return FuncOfAkkerman(n - 1, 1);
+    else
+      return FuncOfAkkerman(n - 1, FuncOfAkkerman(n, m - 1));
+}
+Console.Write(FuncOfAkkerman(2, 3));
---
+++ HW/Seminar_9/Program.cs
@@ -0,0 +1,71 @@
+﻿// Task_1. Задайте значение N Выведите все натуральные числа
+/*
+void ShowNums(int num)
+{
+    if (num > 1) ShowNums(num - 1);
+    Console.Write(num + " ");
+}
+ShowNums(5);
+
+Task_2.
+
+int SumOfDigits(int num)
+{
+    if(num != 0) return SumOfDigits(num / 10) + num % 10;
+    return 0;
+}
+Console.WriteLine(SumOfDigits(1234));
+
+f(1234) -> f(123) + 4 
+f(123) -> f(12) + 3 
+f(12) -> f(1) + 2 
+f(1) -> f(0) + 1
+f(0) -> 0
+f(1) -> 0 + 1 = 1
+f(12) -> 1 + 2 = 3
+f(123) -> 3 + 3 = 6
+f(1234) -> 6 + 4 = 10
+
+// Task_3. Задайте значение M и N. Напишите программу, которая выведeт все натуральные числа 
+// в промежутке от M до N.
+void ShowNums2(int m, int n)
+{
+    if (n > m)
+    {
+        ShowNums2(m, n - 1);
+        Console.Write(n + " ");
+    }
+    if (n < m)
+    {
+        ShowNums2(m, n + 1);
+        Console.Write(n + " ");
+    }
+    if (m == n)
+    {
+        Console.Write(m + " ");
+    }
+}
+ShowNums2(5,1);
+*/
+void ShowNums2(int n, int m)
+{
+     Console.Write(n + " ");
+    if (n > m) ShowNums2(n - 1, m);
+    if (n < m) ShowNums2(n + 1, m);
+}
+ShowNums2(5, 2);
+
+
+/*
+Task_4. Напишите программу, которая на вход принимает два числа А и В, и
+возводит число А в целую степень В.
+
+
+int MultiplyNums(int a, int b)
+{
+    if (b != 0) return MultiplyNums(a, b - 1) * a;
+    return 1;
+}
+
+Console.WriteLine(MultiplyNums(3,2));
+*/
