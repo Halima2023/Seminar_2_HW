e6e34d73c5ad065c904a1818d3d88e6c966cc9f2
Halima <l-ahim@mail.ru>
2023-06-06 01:17:46 +0300

    Task_2

---
+++ HW/HW_9/Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Task_2. Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+        int SumOfDigits (int num1, int num2)
+        {
+            int m = num1;
+            int n = num2;
+            if (m > 0)  return ((m + n)*(n - m + 1) / 2);
+            return 0;
+        }
+        Console.WriteLine(SumOfDigits(4, 8));
